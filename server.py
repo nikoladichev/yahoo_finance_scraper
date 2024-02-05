bbceae5c429ed93d8767de0b8a23f677ea7c4f9f
Nikola Dichev <nikola.dichev@gmail.com>
2024-02-05 21:10:24 +0200

    Successfully running in a container.

--- server.py
+++ server.py
@@ -3,6 +3,10 @@ from yf_scraper import scrape_stock_data, Section;
 
 app = Flask(__name__)
 
+@app.route("/")
+def alive():
+    return "Scraper is up!", 200
+
 @app.route("/<ticker_symbol>/analysis")
 def scrape_summary(ticker_symbol):
     return jsonify(scrape_stock_data(ticker_symbol, Section.ANALYSIS)), 200
